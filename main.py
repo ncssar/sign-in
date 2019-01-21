657a7b8858f382b9fb8dd3b82645b42353344e52
Tom Grundy <caver456@gmail.com>
2019-01-20 21:32:07 -0800

    prelim
    sign in list screen prelim working

--- main.py
+++ main.py
@@ -38,6 +38,9 @@ kivy.require('1.9.1')
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.recyclegridlayout import RecycleGridLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ListProperty
 from kivy.clock import Clock
 
 KV='''
@@ -46,9 +49,24 @@ KV='''
     font_size: 32
     on_press: app.keyDown(self.text)
 
+<MyRV@RecycleView>:
+    viewclass: 'Label'
+    RecycleGridLayout:
+        cols: 1
+        default_size: None, dp(26)
+        default_size_hint: 1, None
+        size_hint_y: None
+        height: self.minimum_height
+        orientation: 'vertical'
+        
 <KeypadScreen>:
     BoxLayout:
         orientation: 'vertical'
+        BoxLayout:
+            height:20
+            Button:
+                text:'a'
+                on_press:app.showList()
         Label:
             id: topLabel
             font_size:28
@@ -151,6 +169,32 @@ KV='''
         Label:
             id: statusLabel
             font_size: 18
+<ListScreen>:
+    BoxLayout:
+        orientation: 'vertical'
+        BoxLayout:
+            height:20
+            Button:
+                text:'a'
+                on_press:app.switchToBlankKeypad()
+        BoxLayout:
+            MyRV:
+                size_hint_x: 0.1
+                # app.idList (etc) does not seem to work; use root. instead
+                data: [{'text':str(x)} for x in root.idList]
+            MyRV:
+                size_hint_x: 0.45
+                data: [{'text':str(x)} for x in root.nameList]
+            MyRV:
+                size_hint_x: 0.15
+                data: [{'text':str(x)} for x in root.timeInList]
+            MyRV:
+                size_hint_x: 0.15
+                data: [{'text':str(x)} for x in root.timeOutList]
+            MyRV:
+                size_hint_x: 0.15
+                data: [{'text':str(x)} for x in root.totalTimeList]
+
 '''
   
 class signinApp(App):
@@ -186,6 +230,22 @@ class signinApp(App):
         self.hide()
         sm.current='keypad'
         
+    def showList(self,*args):
+        theList.idList=["ID"]
+        theList.nameList=["Name"]
+        theList.timeInList=["In"]
+        theList.timeOutList=["Out"]
+        theList.totalTimeList=["Total"]
+        for key in signInDict.keys():
+            print("key="+key+"  item="+str(signInDict[key]))
+            theList.idList.append(key)
+            theList.nameList.append(signInDict[key][0])
+            theList.timeInList.append(signInDict[key][1])
+            theList.timeOutList.append(signInDict[key][2])
+            theList.totalTimeList.append(signInDict[key][3])
+        sm.transition.direction='up'
+        sm.current='theList'
+                    
     def keyDown(self,text):
         print("keyDown called: text="+text)
         if len(text)<3: # number or code; it must be from the keypad
@@ -228,7 +288,7 @@ class signinApp(App):
             elif text=='Sign In Now':
                 # not yet signed in (or out):
                 if self.typed not in signInDict:
-                    signInDict[self.typed]=[self.roster[self.typed],time.time(),0]
+                    signInDict[self.typed]=[self.roster[self.typed],time.time(),0,0]
                     thankyou.ids.statusLabel.text="Signed in at "+time.strftime("%H:%M",time.localtime(signInDict[self.typed][1]))
                     thankyou.ids.nameLabel.text=self.roster[self.typed]
                     sm.current='thankyou'
@@ -240,10 +300,10 @@ class signinApp(App):
                     outTime=time.time()
                     totalTime=outTime-inTime
                     signInDict[self.typed][2]=outTime
+                    signInDict[self.typed][3]=totalTime
                     thankyou.ids.statusLabel.text="Signed in at "+time.strftime("%H:%M",time.localtime(inTime))+"\nSigned out at "+time.strftime("%H:%M",time.localtime(outTime))+"\nTotal time:"+time.strftime("%h hours %m minutes",time.localtime(totalTime))
                     thankyou.ids.nameLabel.text=self.roster[self.typed]
                     sm.current='thankyou'
-                    Clock.schedule_once(self.switchToBlankKeypad,2)
             print(str(signInDict))
                 
                 
@@ -259,16 +319,25 @@ class SignOutScreen(Screen):
 class ThankyouScreen(Screen):
     pass
 
+class ListScreen(Screen):
+    idList=ListProperty(["id"])
+    nameList=ListProperty(["name"])
+    timeInList=ListProperty(["in"])
+    timeOutList=ListProperty(["out"])
+    totalTimeList=ListProperty(["total"])
+
 Builder.load_string(KV)
 sm=ScreenManager()
 sm.add_widget(KeypadScreen(name='keypad'))
 sm.add_widget(SignInScreen(name='signin'))
 sm.add_widget(SignOutScreen(name='signout'))
 sm.add_widget(ThankyouScreen(name='thankyou'))
+sm.add_widget(ListScreen(name='theList'))
 keypad=sm.get_screen('keypad')
 signin=sm.get_screen('signin')
 signout=sm.get_screen('signout')
 thankyou=sm.get_screen('thankyou')
+theList=sm.get_screen('theList')
 signInDict={}
 
 if __name__ == '__main__':
